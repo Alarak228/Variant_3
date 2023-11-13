1ec88d9c5d1d32a1c5d2cfecb1789e5b538f6de1
Alarak228 <146061422+Alarak228@users.noreply.github.com>
2023-11-13 10:11:38 +0300

    Add files via upload

---
+++ Variant_3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+    int step;
+    string chain = "0";
+    cout << "Назначть колличество шагов:";
+    cin >> step;
+    for (int i = 0;i < step;i++) {
+        string pluss = chain;
+        for (int j = 0;j < chain.size();j++) {
+            if (chain[j] == '0') {
+                pluss[j] = '1';
+            }
+            if (chain[j] == '1') {
+                pluss[j] = '0';
+            }
+        }
+        chain.append(pluss);
+        cout << chain << endl<<endl;
+    }
+    return 0;
+}
